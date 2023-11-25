2fb28925cebece405d6f8d7b125b178320c35c43
Badbloodd <144933470+Badbloodd@users.noreply.github.com>
2023-11-25 17:00:51 +0300

    Pz_4_1.py

--- Pz_4_1.py
+++ Pz_4_1.py
@@ -1,8 +1,8 @@
 try:
-    price_per_kg = float(input("Введите цену 1 кг конфет: "))
+    price = float(input("Введите цену 1 кг конфет: "))
 
     for i in range(2, 21, 2):
-        cost = price_per_kg * (i / 10)
+        cost = price * (i / 10)
         print(f"Стоимость {i/10} кг конфет: {cost}")
 except:
     print("Проверьте правильность введеных данных")
